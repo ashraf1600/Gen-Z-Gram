b9dbea76536a0ac72fc8e80e0583e2453ce6be4b
Ashraful Islam <ashrafdcc1502@gmail.com>
2026-07-28 12:23:31 +0600

    login api done

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = [
 
     # Local apps
     'account',
+    'post',
 ]
 
 MIDDLEWARE = [
---
+++ Backend/post/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+import uuid
+from account.models import User
+
+# Create your models here.
+
+
+class PostAttachment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    image = models.ImageField(upload_to='post_attachments')
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='post_attachments')
+    
+
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    body = models.TextField(blank=True)
+
+
+    attachments = models.ManyToManyField(
+        PostAttachment, related_name='posts', blank=True)
+
+    # likes
+    # likes counts
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='posts')
+
+    
+
+
